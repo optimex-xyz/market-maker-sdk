8f51572f94438d689b6570ea54c4a6cbe0506943
Phathdt <phathdt379@gmail.com>
2024-12-09 14:16:27 +0700

    change export type

--- src/signatures/getSignature.ts
+++ src/signatures/getSignature.ts
@@ -6,9 +6,9 @@ import {
   TypedDataDomain,
   verifyTypedData,
   Wallet,
-} from 'ethers';
+} from 'ethers'
 
-import defaultDomain from './domain';
+import defaultDomain from './domain'
 import {
   confirmDepositType,
   confirmPaymentType,
@@ -17,7 +17,7 @@ import {
   presignType,
   rfqAuthenticationTypes,
   selectionType,
-} from './types';
+} from './types'
 
 export enum SignatureType {
   Presign,
@@ -31,16 +31,16 @@ export enum SignatureType {
 }
 
 function getSignatureType(type: SignatureType): any {
-  if (type === SignatureType.Presign) return presignType;
-  else if (type === SignatureType.ConfirmDeposit) return confirmDepositType;
-  else if (type === SignatureType.SelectPMM) return selectionType;
-  else if (type === SignatureType.VerifyingContract) return selectionType;
-  else if (type === SignatureType.RFQ) return rfqAuthenticationTypes;
-  else if (type === SignatureType.MakePayment) return makePaymentType;
-  else if (type === SignatureType.ConfirmPayment) return confirmPaymentType;
+  if (type === SignatureType.Presign) return presignType
+  else if (type === SignatureType.ConfirmDeposit) return confirmDepositType
+  else if (type === SignatureType.SelectPMM) return selectionType
+  else if (type === SignatureType.VerifyingContract) return selectionType
+  else if (type === SignatureType.RFQ) return rfqAuthenticationTypes
+  else if (type === SignatureType.MakePayment) return makePaymentType
+  else if (type === SignatureType.ConfirmPayment) return confirmPaymentType
   else if (type === SignatureType.ConfirmSettlement)
-    return confirmSettlementType;
-  else throw new Error('Invalid signature type!');
+    return confirmSettlementType
+  else throw new Error('Invalid signature type!')
 }
 
 export async function getSigner(
@@ -51,20 +51,20 @@ export async function getSigner(
   type: SignatureType,
   signature: string
 ) {
-  const values = { tradeId: tradeId, infoHash: infoHash };
+  const values = { tradeId: tradeId, infoHash: infoHash }
   const contractDomain: TypedDataDomain = await defaultDomain(
     signerHelper,
     provider
-  );
+  )
   return verifyTypedData(
     contractDomain,
     getSignatureType(type),
     values,
     signature
-  );
+  )
 }
 
-export default async function getSignature(
+export async function getSignature(
   Signer: Signer | Wallet,
   provider: Provider,
   signerHelper: AddressLike,
@@ -76,15 +76,15 @@ export default async function getSignature(
   const contractDomain: TypedDataDomain = await defaultDomain(
     signerHelper,
     provider
-  );
+  )
 
-  let values: any;
-  if (type === SignatureType.MakePayment) values = { infoHash };
-  else values = { tradeId: tradeId, infoHash: infoHash };
+  let values: any
+  if (type === SignatureType.MakePayment) values = { infoHash }
+  else values = { tradeId: tradeId, infoHash: infoHash }
 
   return await Signer.signTypedData(
     domain ?? contractDomain,
     getSignatureType(type),
     values
-  );
+  )
 }
