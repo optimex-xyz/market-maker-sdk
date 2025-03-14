eb5a45011ee589841eed9e48a94aa5a373d0cc9a
Phathdt <phathdt379@gmail.com>
2025-03-14 19:32:53 +0700

    feat: update domain optimex.xyz

--- src/config/config.ts
+++ src/config/config.ts
@@ -25,7 +25,7 @@ const environments: Record<Environment, EnvironmentConfig> = {
     },
   },
   production: {
-    backendUrl: 'https://api.optimex.cc',
+    backendUrl: 'https://api.optimex.xyz',
     rpcUrl: 'https://bitfi-ledger-testnet.alt.technology',
     routerAddress: '0x272599CE3602A49B580A5C4a4d3C1067E30248D2',
     paymentAddressMap: {
