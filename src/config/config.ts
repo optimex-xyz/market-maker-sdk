d10444f1140cef29896e0fc2723ff444fcd08586
phathdt <phathdt379@gmail.com>
2025-08-29 11:08:53 +0700

    feat: update payment liquid address

--- src/config/config.ts
+++ src/config/config.ts
@@ -25,7 +25,7 @@ const environments: Record<Environment, EnvironmentConfig> = {
       ethereum_sepolia: '0x1d8b58438D5Ccc8Fcb4b738C89078f7b4168C9c0',
     },
     liquidationAddressMap: {
-      ethereum_sepolia: '0x5A0EAc7b85F08b50f04C89A920F1E834d24694E3',
+      ethereum_sepolia: '0x2AEDAF2470EfFD35B295D710fB2e5FDAdE642B0A',
     },
   },
   staging: {
